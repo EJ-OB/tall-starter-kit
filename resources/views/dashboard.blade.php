81eabbf9bfac4d4ee48390340ff1e0cf702bff3d
EJ Beron <ejberon@objectbright.com>
2025-03-02 04:30:25 +0800

    chore: update sample toasts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,46 @@
 <x-layouts.app>
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <button x-data @click="new Toast().title('Toastify!!!').seconds(3).send()" class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+        <button
+            x-data
+            @click="
+                new Toast()
+                    .title('Danger')
+                    .message('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
+                    .variant('danger')
+                    .seconds(3)
+                    .send();
+
+                new Toast()
+                    .title('Info')
+                    .message('It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.')
+                    .variant('info')
+                    .seconds(3)
+                    .send();
+
+                new Toast()
+                    .title('Success')
+                    .message('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
+                    .variant('success')
+                    .seconds(3)
+                    .send();
+
+                new Toast()
+                    .title('Warning')
+                    .message('Lorem Ipsum is simply dummy text of the printing and typesetting industry.')
+                    .variant('warning')
+                    .seconds(3)
+                    .send();
+
+                new Toast()
+                    .title('Custom Icon')
+                    .message('Custom icon example')
+                    .icon('video-camera')
+                    .variant('info')
+                    .seconds(3)
+                    .send();
+            "
+            class="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+        >
             Notify
         </button>
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
--- resources/views/livewire/toasts.blade.php
+++ resources/views/livewire/toasts.blade.php
@@ -1,12 +1,6 @@
 @use(App\Enums\ToastVariant)
 
 <div class="flex flex-col-reverse z-50 fixed top-0 right-0 min-w-72 max-w-72 mt-8 me-8 gap-y-2 overflow-hidden" role="status">
-    {{ \App\Toast\Toast::make(['id' => '123', 'title' => 'Danger', 'variant' => ToastVariant::Danger]) }}
-    {{ \App\Toast\Toast::make(['id' => '123', 'title' => 'Custom Icon', 'variant' => ToastVariant::Success, 'icon' => 'bars-3']) }}
-    {{ \App\Toast\Toast::make(['id' => '123', 'title' => 'Danger', 'message' => 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum', 'variant' => ToastVariant::Danger]) }}
-    {{ \App\Toast\Toast::make(['id' => '123', 'title' => 'Info', 'message' => 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum', 'variant' => ToastVariant::Info]) }}
-    {{ \App\Toast\Toast::make(['id' => '123', 'title' => 'Success', 'message' => 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum', 'variant' => ToastVariant::Success]) }}
-    {{ \App\Toast\Toast::make(['id' => '123', 'title' => 'Warning', 'message' => 'Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum', 'variant' => ToastVariant::Warning]) }}
     @foreach ($toasts as $toast)
         {{ $toast }}
     @endforeach
