083e5a774c9e1bedcabeb3db259b56c1df98d3be
EJ Beron <ejberon@objectbright.com>
2025-03-03 15:49:35 +0800

    fix: fix flickering when redirecting

--- modules/Toast/resources/views/livewire/toasts.blade.php
+++ modules/Toast/resources/views/livewire/toasts.blade.php
@@ -1,7 +1,11 @@
 @use(Modules\Toast\Enums\ToastVariant)
 
-<div class="flex flex-col-reverse z-50 fixed top-0 right-0 min-w-72 max-w-72 mt-8 me-8 gap-y-2 overflow-hidden" role="status">
-    @foreach ($toasts as $toast)
-        {{ $toast }}
-    @endforeach
+<div>
+    @if($toasts->isNotEmpty())
+        <div x-cloak class="flex flex-col-reverse z-50 fixed top-0 right-0 min-w-72 max-w-72 mt-8 me-8 gap-y-2 overflow-hidden" role="status">
+            @foreach ($toasts as $toast)
+                {{ $toast }}
+            @endforeach
+        </div>
+    @endif
 </div>
