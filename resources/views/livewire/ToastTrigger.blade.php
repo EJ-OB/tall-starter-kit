34b2913658c67f59693e30213f4a968ff3669846
EJ Beron <ejberon@objectbright.com>
2025-03-03 15:51:49 +0800

    chore: update sample toast trigger component

--- resources/views/livewire/ToastTrigger.blade.php
+++ resources/views/livewire/ToastTrigger.blade.php
@@ -2,15 +2,19 @@
 
 use Modules\Toast\Toast;
 
-$toast = fn () => Toast::make()
-    ->icon('check')
-    ->variant(\Modules\Toast\Enums\ToastVariant::Success)
-    ->title('Operation successful')
-    ->send();
+$toast = function () {
+    Toast::make()
+        ->icon('check')
+        ->variant(\Modules\Toast\Enums\ToastVariant::Success)
+        ->title('Operation successful')
+        ->send();
+
+    $this->redirectRoute('users.index');
+};
 ?>
 
 <div>
     <flux:button wire:click="toast">
-        Trigger toast from a livewire component
+        Trigger toast from a livewire component and redirect
     </flux:button>
 </div>
