e15eb3bd864c38ccf7cea56b34c41da2fdf4e680
EJ Beron <ejberon@objectbright.com>
2025-03-04 14:30:14 +0800

    style: fix shadow

--- modules/Toast/resources/views/livewire/toasts.blade.php
+++ modules/Toast/resources/views/livewire/toasts.blade.php
@@ -2,7 +2,7 @@
 
 <div>
     @if($toasts->isNotEmpty())
-        <div x-cloak class="flex flex-col-reverse z-50 fixed top-0 right-0 min-w-72 max-w-72 mt-8 me-8 gap-y-2 overflow-hidden" role="status">
+        <div x-cloak class="flex flex-col-reverse z-50 fixed top-0 right-0 min-w-72 max-w-72 mt-8 me-8 gap-y-2" role="status">
             @foreach ($toasts as $toast)
                 {{ $toast }}
             @endforeach
